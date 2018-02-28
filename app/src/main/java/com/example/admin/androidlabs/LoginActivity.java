bce6739fe40ed1faeb1f4e922965a32cccfe29b0
sing0888 <sing0888@algonquinlive.com>
2018-02-28 14:44:46 -0500

    initial commit

---
+++ app/src/main/java/com/example/admin/androidlabs/LoginActivity.java
@@ -0,0 +1,169 @@
+package com.example.admin.androidlabs;
+
+
+
+
+
+
+import android.app.Activity;
+
+import android.content.Context;
+
+import android.content.Intent;
+
+import android.content.SharedPreferences;
+
+import android.os.Bundle;
+
+import android.util.Log;
+
+import android.view.View;
+
+import android.widget.Button;
+
+import android.widget.EditText;
+
+import android.widget.TextView;
+
+
+
+public class LoginActivity extends Activity {
+
+
+
+    protected static final String ACTIVITY_NAME = "Login Activity";
+
+
+
+    EditText edtLogin,edtPassword;
+
+    TextView loginText;
+
+
+
+
+
+
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_login);
+
+        Log.i(ACTIVITY_NAME,"In onCreate()");
+
+        Button loginButton = (Button) findViewById(R.id.button2);
+
+        edtLogin = (EditText) findViewById(R.id.email);
+
+        edtPassword = (EditText) findViewById(R.id.password);
+
+
+
+        final SharedPreferences sharedPreferences = getSharedPreferences("login", Context.MODE_PRIVATE);
+
+        edtLogin.setText(sharedPreferences.getString("DefaultEmail", "email@domain.com"));
+        edtPassword.setText(sharedPreferences.getString("DefaultEmail", "email@domain.com"));
+        loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+
+                editor.putString("DefaultEmail", edtLogin.getText().toString());
+                editor.putString("DefaultEmail", edtPassword.getText().toString());
+                editor.commit();
+
+                Intent intent = new Intent(LoginActivity.this, StartActivity.class);
+
+              startActivityForResult(intent, 275);
+
+
+
+            }
+
+        });
+
+
+
+    }
+
+
+
+    @Override
+
+    protected void onStart() {
+
+        super.onStart();
+
+        Log.i(ACTIVITY_NAME, "In onStart()");
+
+
+
+    }
+
+
+
+    @Override
+
+    protected void onResume() {
+
+        super.onResume();
+
+        Log.i(ACTIVITY_NAME,"In onResume()");
+
+
+
+    }
+
+
+
+    @Override
+
+    protected void onStop() {
+
+        super.onStop();
+
+        Log.i(ACTIVITY_NAME,"In onStop()");
+
+
+
+    }
+
+
+
+    @Override
+
+    protected void onDestroy() {
+
+        super.onDestroy();
+
+        Log.i(ACTIVITY_NAME,"In onDestroy()");
+
+
+
+    }
+
+
+
+    @Override
+
+    protected void onPause() {
+
+        super.onPause();
+
+        Log.i(ACTIVITY_NAME,"In onPause()");
+
+
+
+    }
+
+}
+
+
+
